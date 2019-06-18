22dc1c5e9f69d050121c00c5c24bbb9c24a1a45b
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-06-18 10:54:03 +0900

    Jobモデルの実装

---
+++ model/Job.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Job(Enum):
+    VILLAGER = 1    # 村人
+    SEER = 2        # 占い師
+    MEDIUM = 3      # 霊媒師
+    HUNTER = 4      # 狩人
+    MASON = 5       # カップル
+    MADMAN = 6      # 狂人
+    WEREWOLF = 7    # 人狼
+    WEREHAMSTER = 8 # 妖狐
