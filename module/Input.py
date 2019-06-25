149197efa0c67c168e28cc4ed691fef0a25748b5
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-06-25 11:56:04 +0900

    csvから順番に発言を取得

--- module/Input.py
+++ module/Input.py
@@ -1,7 +1,17 @@
 import sys
+import pandas as pd
 
 class Input:
     def getByCmdline(self):
         print("input:", end="")
         str = input()
         return str
+    def getByCsvFile(self, filePath):
+        self.df = pd.read_csv(filePath, sep=',', names=('何日目', 'プレイヤー名', '役職', '発言'))
+        self.index = 0
+    def getNextCsv(self):
+        if(len(self.df) < self.index+1):
+            return None
+        res = self.df[self.index:self.index+1]
+        self.index += 1
+        return res
