f68a38bc1f066266d71335ec0a8c10617a576eb3
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-07-02 08:02:47 +0900

    CSVの列を指定して取得

--- module/Input.py
+++ module/Input.py
@@ -6,12 +6,33 @@ class Input:
         print("input:", end="")
         str = input()
         return str
-    def getByCsvFile(self, filePath):
+    def setCsvFile(self, filePath):
         self.df = pd.read_csv(filePath, sep=',', names=('何日目', 'プレイヤー名', '役職', '発言'))
         self.index = 0
-    def getNextCsv(self):
+    def getCsv(self):
         if(len(self.df) < self.index+1):
             return None
         res = self.df[self.index:self.index+1]
-        self.index += 1
         return res
+
+    # 列を指定して値を取得
+    def getDay(self):
+        if(len(self.df) < self.index+1):
+            return None
+        return self.df.iat[self.index, 0]
+    def getPlayerName(self):
+        if(len(self.df) < self.index+1):
+            return None
+        return self.df.iat[self.index, 1]
+    def getRole(self):
+        if(len(self.df) < self.index+1):
+            return None
+        return self.df.iat[self.index, 1]
+    def getTalk(self):
+        if(len(self.df) < self.index+1):
+            return None
+        return self.df.iat[self.index, 3]
+
+    # 次の行へ
+    def next(self):
+        self.index += 1
