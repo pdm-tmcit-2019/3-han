fecaf16ed5d220f29532a62fd37f032837b95a45
machibito2 <bookslover444@gmail.com>
2019-07-16 10:06:46 +0900

    モジュール化

--- module/SyntacticAnalysis.py
+++ module/SyntacticAnalysis.py
@@ -1,20 +1,27 @@
 import sys
 import CaboCha
 
-testSentence = "太郎はこの本を二郎を見た女性に渡した。"
+testSentence = "COします"
 
-cabocha = CaboCha.Parser("-u /mnt/c/Users/machibito2/Dropbox/school/4-grade/zemi/MultipleLengthFloatingPointCalc/3-han/user.dic -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
-# -u (ユーザー辞書の絶対パス) -d (ipadicとかのあるディレクトリの絶対パス(いるのであれば)(あると強そう))
 
-def syntacsAnalysis(inputSentence):
-    tree =  cabocha.parse(inputSentence)
-    chunkId = 0
-    for i in range(0, tree.size()):
-        token = tree.token(i)
-        if token.chunk != None:
-            print(chunkId, token.chunk.link, token.chunk.head_pos,token.chunk.func_pos, token.chunk.score)
-            chunkId += 1
+class SyntacsAnalysis:
+    sentence = ""
+    cabocha = CaboCha.Parser("-u /mnt/c/Users/machibito2/Dropbox/school/5-grade/jinrou/3-han/user.dic -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
+    # -u (ユーザー辞書の絶対パス) -d (ipadicとかのあるディレクトリの絶対パス(いるのであれば)(あると強そう))
 
-        print(token.surface, token.feature, token.ne)
+    def __init__(self, sentence):
+        self.sentence = sentence
 
-syntacsAnalysis(testSentence);
+    def syntacsAnalysis(self):
+        tree =  self.cabocha.parse(self.sentence)
+        chunkId = 0
+        for i in range(0, tree.size()):
+            token = tree.token(i)
+            if token.chunk != None:
+                print(chunkId, token.chunk.link, token.chunk.head_pos,token.chunk.func_pos, token.chunk.score)
+                chunkId += 1
+
+            print(token.surface, token.feature, token.ne)
+
+hello = SyntacsAnalysis(testSentence)
+hello.syntacsAnalysis()
