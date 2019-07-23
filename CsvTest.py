4a07ae60034a8c500baa0a791f1e121ff202dd1f
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-07-23 10:57:07 +0900

    形態素解析まで

--- CsvTest.py
+++ CsvTest.py
@@ -1,4 +1,5 @@
 from module import Input
+from module import MorphologicalAnalysis
 import io,sys
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
@@ -10,13 +11,19 @@ while True:
     # CSV読み込み
     nowDay = input.getDay()
     talkPlayerName = input.getPlayerName()
-    talkWord = input.getTalk()
+    talkSentence = input.getTalk()
 
     # 読み込めなかったら終了
     if nowDay is None:
         break
 
+    # 形態素解析
+    morphologicalAnalysis = MorphologicalAnalysis.MorphologicalAnalysis(talkSentence)
+    aftereMorphologicalAnalysisSentence = morphologicalAnalysis.analysis()
 
-    print(nowDay, talkPlayerName, talkWord)
+    # print(nowDay, talkPlayerName, aftereMorphologicalAnalysisSentence)
+    for now in aftereMorphologicalAnalysisSentence:
+        print(now.word)
     # 次のログへ
+    # break
     input.next()
