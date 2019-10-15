f3f652706eaa3b8c7c734ba63877969e006ee5f7
Ryuki <ryuki@meuron.jp>
2019-10-15 09:49:18 +0900

    エンドポイントの変更

--- node-server/src/server.ts
+++ node-server/src/server.ts
@@ -16,7 +16,7 @@ class Server {
 		const server = this.http.Server(app)
 		app.use(this.express.static(this.path.join(__dirname, '/../public')))
 
-		app.get('/', (req: Request, res: Response, next: NextFunction) => {
+		app.get('/test', (req: Request, res: Response, next: NextFunction) => {
 			var data = this.fs.readFileSync(this.path.join(__dirname, '/../public/index.htm'))
 			res.end(data)
 		})
