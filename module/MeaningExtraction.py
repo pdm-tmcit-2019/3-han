771c9f5a7a17033b9039b1557d2b415fffce4ed5
machibito2 <bookslover444@gmail.com>
2019-12-23 23:50:36 +0900

    噛み先と守り先と霊媒先の決定実装

--- module/MeaningExtraction.py
+++ module/MeaningExtraction.py
@@ -1,9 +1,16 @@
 # coding: utf8
-
+import random
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from module import SyntacticAnalysis
 
 class MeaningExtraction:
     ROLE_LIST = ["村人", "占い師", "霊媒師", "狩人", "双子", "狂人", "人狼", "妖狐"]
     FIRST_PERSON_LIST = ["私", "わたし", "僕", "ぼく"]
+    HUMAN_ROLE_LIST = ["村人", "占い師", "霊媒師", "狩人", "双子"]
+    ENEMY_ROLE_LIST = ["狂人", "人狼", "妖狐"]
+    
     def checkCo(self, afterSyntacsAnalysis):
         sentence = u""
         for nowAnal in afterSyntacsAnalysis:
@@ -16,3 +23,38 @@ class MeaningExtraction:
                     if sentence.find(self.FIRST_PERSON_LIST[indexFirst]) != -1:
                         return index
         return -1
+
+    def decideMediumTo(self, beforeDeadPeople):
+        # 霊媒対象は毎ターン1人しか出ない
+        return beforeDeadPeople;
+
+    def decideProtectTo(self, alivePeople, beforeProtected):
+        protectTarget = []
+        for index in range(len(alivePeople)):
+            # 前に守ってない人の中で生きてる人
+            if index != beforeProtected and alivePeople[index] == 1:
+                protectTarget.append(index)
+        return protectTarget[random.randint(0, len(protectTarget)-1)]
+
+    def decideKillTo(self, nowRoleList):
+        killTarget = []
+        for index in range(len(nowRoleList)):
+            for enemyRole in self.ENEMY_ROLE_LIST:
+                if(nowRoleList[index] == enemyRole):
+                    killTarget.append(index)
+                    break
+        return killTarget[random.randint(0, len(killTarget)-1)]
+
+#テスト
+
+testSentence = "私は人狼です"
+res = SyntacticAnalysis.SyntacsAnalysis(testSentence).syntacsAnalysis()
+
+alivePeople = [1, 1, 0, 1, 1, 0, 0, 0, 1]
+nowRoleList = ["村人", "占い師", "霊媒師", "狩人", "双子", "狂人", "人狼", "妖狐"]
+
+test = MeaningExtraction()
+print(test.checkCo(res))
+print(test.decideMediumTo(5))
+print(test.decideProtectTo(alivePeople, 1))
+print(nowRoleList[test.decideKillTo(nowRoleList)])
---
+++ test.py
@@ -0,0 +1,60 @@
+# coding: utf8
+
+import sys
+import CaboCha
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from model import Clauses
+
+testSentence = "machibito2ですか?"
+myUserName = "machibito2"
+
+class SyntacsAnalysis:
+    sentence = ""
+    cabocha = CaboCha.Parser() #("-u ../user.dic -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
+    # -u (ユーザー辞書の相対パス) -d (ipadicとかのあるディレクトリの絶対パス(いるのであれば)(あると強そう))
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    def syntacsAnalysis(self):
+        result = []
+        tmpStr = ""
+        tmpChunkID = ""
+        tmpChunkLink = ""
+        tmpChunkScore = ""
+        tree = self.cabocha.parse(self.sentence)
+        chunkId = 0
+        for i in range(0, tree.size()):
+            token = tree.token(i)
+            if token.chunk != None:
+                if tmpStr != "":
+                    # print(tmpStr)
+                    clauses = Clauses.Clauses(tmpStr, tmpChunkID, tmpChunkLink, tmpChunkScore)
+                    # result.append([chunkId, token.chunk.link, token.chunk.head_pos,token.chunk.func_pos, token.chunk.score])
+                    result.append(clauses)
+                    tmpStr = ""
+                # print(chunkId, token.chunk.link, token.chunk.score)
+                tmpChunkID = chunkId
+                tmpChunkLink = token.chunk.link
+                tmpChunkScore = token.chunk.score
+                chunkId += 1
+            # print(token.surface, token.feature)
+            tmpStr += token.surface
+            # print(token.surface)
+        clauses = Clauses.Clauses(tmpStr, tmpChunkID, tmpChunkLink, tmpChunkScore)
+        result.append(clauses)
+        # print(tmpStr)
+        return result
+
+hello = SyntacsAnalysis(testSentence)
+# print(hello.syntacsAnalysis())
+res = hello.syntacsAnalysis()
+for resu in res:
+    print(resu.clause, resu.myID, resu.toID, resu.score)
+
+if res[-1].clause[-1:] == "?":
+	print("a")	#疑問
+	for resu in res:
+		if myUserName in resu.clause:
+			print("ぼくはmachibito2です")	#自分への疑問
