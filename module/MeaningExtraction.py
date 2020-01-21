9a76178cb6024af328a0a5d35e0c380dead2e1ab
machibito2 <bookslover444@gmail.com>
2020-01-21 13:15:41 +0900

    噛み先と吊り先のバグ修正

--- module/MeaningExtraction.py
+++ module/MeaningExtraction.py
@@ -1,4 +1,4 @@
-﻿#coding: utf8
+# coding: utf8
 import random
 import sys
 import os
@@ -29,7 +29,7 @@ class MeaningExtraction:
         # 霊媒対象は毎ターン1人しか出ないから吊った人
         return beforeDeadPeople;
 
-　　　　# 自分が狩人で守る対象決定
+    # 自分が狩人で守る対象決定
     def decideProtectTo(self, alivePeople, beforeProtected):
         protectTarget = []
         for index in range(len(alivePeople)):
@@ -46,32 +46,33 @@ class MeaningExtraction:
                 if(nowRoleList[index] == enemyRole):
                     killTarget.append(index)
                     break
-	if(len(killTarget) == 0):
-		killTarget.append(random.randint(0, len(nowRoleList)))
+        if(len(killTarget) == 0):
+            killTarget.append(random.randint(0, len(nowRoleList)))
         return killTarget[random.randint(0, len(killTarget)-1)]
-	
+
     # 噛み先決定
     def decideBiteTo(self, nowRoleList):
-	biteTarget = []
-	for index in range(len(nowRoleList)):
-		for enemyRole in self.ENEMY_ROLE_LIST:
-			if(nowRoleList[index] != enemyRole):
-				biteTarget.append(index)
-				break
-	if(len(biteTarget) == 0):
-		biteTarget.append(random.randint(0, len(nowRoleList)))
-	return biteTarget[randm.randint(0, len(biteTarget)-1)]
+        biteTarget = []
+        for index in range(len(nowRoleList)):
+            for humanRole in self.HUMAN_ROLE_LIST:
+                if(nowRoleList[index] == humanRole):
+                    biteTarget.append(index)
+                    break
+        if(len(biteTarget) == 0):
+            biteTarget.append(random.randint(0, len(nowRoleList)))
+        return biteTarget[random.randint(0, len(biteTarget)-1)]
 
 #テスト
 
-#testSentence = "私は人狼です"
-#res = SyntacticAnalysis.SyntacsAnalysis(testSentence).syntacsAnalysis()
+# testSentence = "私は人狼です"
+# res = SyntacticAnalysis.SyntacsAnalysis(testSentence).syntacsAnalysis()
 
-#alivePeople = [1, 1, 0, 1, 1, 0, 0, 0, 1]
-#nowRoleList = ["村人", "占い師", "霊媒師", "狩人", "双子", "狂人", "人狼", "妖狐"]
+# alivePeople = [1, 1, 0, 1, 1, 0, 0, 0, 1]
+# nowRoleList = ["村人", "占い師", "霊媒師", "狩人", "双子", "狂人", "人狼", "妖狐"]
 
-#test = MeaningExtraction()
-#print(test.checkCo(res))
-#print(test.decideMediumTo(5))
-#print(test.decideProtectTo(alivePeople, 1))
-#print(nowRoleList[test.decideKillTo(nowRoleList)])
+# test = MeaningExtraction()
+# print(test.checkCo(res))
+# print(test.decideMediumTo(5))
+# print(test.decideProtectTo(alivePeople, 1))
+# print(nowRoleList[test.decideKillTo(nowRoleList)])
+# print(nowRoleList[test.decideBiteTo(nowRoleList)])
