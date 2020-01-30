ffe6761b76ca2b75e31beb99b071f3b1282f286d
ryuji sanada <m15078@g.metro-cit.ac.jp>
2020-01-30 22:45:45 +0900

    Scrollの作成
    他
    - OnymousAudienceBoardの修正

--- model/api/request/Scroll.py
+++ model/api/request/Scroll.py
@@ -1,2 +1,19 @@
-class AnonymousAudienceChat:
-	def __init__(): 
+# coding: utf8
+import json
+
+class Scroll:
+	my_character_name = None
+	my_character_role = None
+
+	def __init__(self):
+		self.my_character_name = "villager1"
+		self.my_character_role = "Villager"
+
+	def get(self):
+		f = open('model/api/request/jsonld/scroll.jsonld', encoding="utf_8")
+		json_data = json.load(f)
+		json_data["myCharacter"]["name"]["en"] = self.my_character_name
+		json_data["myCharacter"]["name"]["ja"] = self.my_character_name
+		json_data["myCharacter"]["role"]["name"]["en"] = self.my_character_role
+		json_data["myCharacter"]["role"]["name"]["ja"] = self.my_character_role
+		return str(json_data)
