9dc6c3a8cbb95f5899a01fc08c61f36373e27950
ryuji sanada <m15078@g.metro-cit.ac.jp>
2020-01-30 23:00:55 +0900

    Starの作成
    他
    - Scrollの修正

--- model/api/request/Star.py
+++ model/api/request/Star.py
@@ -1,2 +1,20 @@
-class AnonymousAudienceChat:
-	def __init__(): 
+# coding: utf8
+import json
+
+class Star:
+	my_character_name = None
+	my_character_role = None
+
+	def __init__(self):
+		self.my_character_name = "Adil"
+		self.my_character_role = "Villager"
+
+	def get(self, is_marked):
+		f = open('model/api/request/jsonld/star.jsonld', encoding="utf_8")
+		json_data = json.load(f)
+		json_data["myCharacter"]["name"]["en"] = self.my_character_name
+		json_data["myCharacter"]["name"]["ja"] = self.my_character_name
+		json_data["myCharacter"]["role"]["name"]["en"] = self.my_character_role
+		json_data["myCharacter"]["role"]["name"]["ja"] = self.my_character_role
+		json_data["star"]["isMarked"] = is_marked
+		return str(json_data)
